41be16313867169fb4472880789c848d3815386e
BrRoman <editeur@traditions-monastiques.com>
2020-12-16 15:53:21 +0100

    Listing hôtellerie OK.

--- hotellerie/apps/listings/views.py
+++ hotellerie/apps/listings/views.py
@@ -14,13 +14,14 @@ from modules.dates import date_to_french_string
 from apps.parloirs.models import Parloir
 from apps.sejours.models import Sejour
 
+DAYS = {}
+TODAY = date.today()
+
 
 def cuisine(request):
     """ Listing cuisine. """
-    days = {}
-    today = date.today()
     for i in range(15):
-        day = today + timedelta(days=i)
+        day = TODAY + timedelta(days=i)
         day_string = date_to_french_string(day)
         hote = is_first_repas = is_last_repas = is_monorepas = ''
         table_hotes_midi, \
@@ -237,7 +238,7 @@ def cuisine(request):
                 total_parloirs_soir += (parloir.nombre + 1)
 
         # On compile le tout pour le jour concerné :
-        days[day] = {
+        DAYS[day] = {
             'day_string': day_string,
             'midi': {
                 'table_hotes': table_hotes_midi,
@@ -265,7 +266,7 @@ def cuisine(request):
             },
         }
 
-    return render(request, 'listings/cuisine.html', {'days': days})
+    return render(request, 'listings/cuisine.html', {'days': DAYS})
 
 
 def hotellerie(request):
@@ -274,14 +275,80 @@ def hotellerie(request):
     # Settings:
     width, height = A4
     pdf = canvas.Canvas(buffer, pagesize=A4, bottomup=0)
-    pdf.setFont("Helvetica", 10)
+    pdf.setFont("Courier", 10)
     pdf.saveState()
     pdf.setLineWidth(0.2)
 
-    pdf.drawString(100, 100, "Listing hôtellerie")
-    pdf.drawString(100, 100, "Listing hôtellerie")
-    pdf.drawString(100, 100, "Listing hôtellerie")
-    pdf.drawString(100, 100, "Listing hôtellerie")
+    # Title:
+    coord_y = 50
+    pdf.drawCentredString(width/2.0, coord_y, "+")
+    coord_y += 15
+    pdf.drawCentredString(width/2.0, coord_y, "PAX")
+    coord_y += 15
+    pdf.drawCentredString(width/2.0, coord_y, "LISTING HÔTELLERIE")
+
+    # Parloirs:
+    coord_y += 35
+    pdf.drawCentredString(width/2.0, coord_y, "PARLOIRS")
+    coord_y += 10
+    parloirs = Parloir.objects.filter(
+        date__gte=TODAY
+    ) & Parloir.objects.filter(
+        date__lte=TODAY + timedelta(days=15)
+    ).order_by('date')
+    for index, parloir in enumerate(parloirs):
+        coord_y += 15
+        line = ''
+        line += '- {}'.format(
+            date_to_french_string(parloir.date))
+        line += ' ({})'.format(parloir.repas) if parloir.repas else ''
+        line += ' : {}'.format(parloir.personne.__str__())
+        line += ' + {}'.format(parloir.nombre) if parloir.nombre else ''
+        line += ' (repas apporté)' if parloir.repas_apporte else ''
+        pdf.drawString(40, coord_y, line)
+        line = 'Parloir : {}'.format(
+            parloir.parloir) if parloir.parloir and parloir.parloir != 'Non défini' else ''
+        if line:
+            coord_y += 15
+            pdf.drawString(60, coord_y, line)
+
+    # Hôtes:
+    coord_y += 35
+    pdf.drawCentredString(width/2.0, coord_y, "HÔTES")
+    coord_y += 10
+    sejours = ((Sejour.objects.filter(
+        sejour_du__lte=TODAY
+    ) & Sejour.objects.filter(
+        sejour_au__gte=TODAY
+    )) | (Sejour.objects.filter(
+        sejour_du__lte=TODAY
+    ) & Sejour.objects.filter(
+        sejour_au=TODAY
+    )) | (Sejour.objects.filter(
+        sejour_du=TODAY
+    ) & Sejour.objects.filter(
+        sejour_au__gte=TODAY
+    )) | (Sejour.objects.filter(
+        sejour_du__gte=TODAY
+    ) & Sejour.objects.filter(
+        sejour_du__lte=TODAY + timedelta(days=15)
+    ))).order_by('sejour_du', 'sejour_au')
+    for index, sejour in enumerate(sejours):
+        coord_y += 15
+        line = ''
+        line += '- {}'.format(sejour.personne.__str__())
+        pdf.drawString(40, coord_y, line)
+        coord_y += 15
+        line = 'Du {} ({}) au {} ({})'.format(
+            date_to_french_string(sejour.sejour_du),
+            sejour.repas_du,
+            date_to_french_string(sejour.sejour_au),
+            sejour.repas_au,
+        )
+        pdf.drawString(60, coord_y, line)
+        coord_y += 15
+        line = 'Chambre(s) : {}'.format(sejour.chambres_string())
+        pdf.drawString(60, coord_y, line)
 
     pdf.showPage()
     pdf.save()

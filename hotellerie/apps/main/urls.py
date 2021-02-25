bcac981f055af7cd10691a26a1cac9a917e262b2
BrRoman <editeur@traditions-monastiques.com>
2021-02-25 10:31:46 +0100

    Calendar moved to the main app.

--- hotellerie/apps/main/urls.py
+++ hotellerie/apps/main/urls.py
@@ -1,10 +1,13 @@
 """ apps/main/urls.py """
 
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
 app_name = 'main'
 urlpatterns = [
     path('', views.home, name='home'),
+    path('calendar/', views.calendar, name='calendar'),
+    re_path(r'^calendar/(?P<day>\d{2})/(?P<month>\d{2})/(?P<year>\d{4})/$',
+            views.calendar, name='calendar'),
 ]

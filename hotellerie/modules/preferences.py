e85bc5fca7d2907fcffef7a4cb6d79ffa5452d55
BrRoman <editeur@traditions-monastiques.com>
2021-11-13 17:32:38 +0100

    Preferences updated.

--- hotellerie/modules/preferences.py
+++ hotellerie/modules/preferences.py
@@ -3,5 +3,5 @@
 PREFERENCES = {
     'mail_hotelier': 'hotellerie@clairval.com',
     'mail_sacristain': 'sacristie@clairval.com',
-    'mail_services': 'editeur@traditions-monastiques.com',
+    'mail_services': 'photogravure@traditions-monastiques.com',
 }
